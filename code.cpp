5ba1af6d09ddeb448d55c63e10f7a6e0ea4d3862
Xuân Quỳnh <95061764+xuanquynh-limerence@users.noreply.github.com>
2021-12-01 16:27:54 +0700

    practice

---
+++ code.cpp
@@ -0,0 +1,27 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+###End banned keyword*/
+
+#include <iostream>
+
+//###INSERT CODE HERE -
+int input (int &x)
+{
+    cin >> x;
+    return x;
+}
+
+int input()
+{
+    int x;
+    cin >> x;
+    return x;
+}
+//end
+
+int main(){
+    int a, b;
+    a=input();
+    input(b);
+    std::cout << TongUocChung(a, b);
+    return 0;
+}
